801ae2a86353771065451aaa5d33e586c0d3f635
Sl33pySage <52090741+Sl33pySage@users.noreply.github.com>
2026-05-19 02:05:44 -0500

    Getting ready to start by reading the little book of text game programming

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Please enter your name:");
+
+    let mut name: String = String::new();
+
+    io::stdin()
+        .read_line(&mut name)
+        .expect("Failed to read line");
+
+    println!("Hello, {name}");
 }
