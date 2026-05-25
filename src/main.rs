4f33d23e45b256ffc5772c25d1d428206bb1ef01
Sl33pySage <52090741+Sl33pySage@users.noreply.github.com>
2026-05-25 00:17:56 -0500

    Gotta research on structs and implementing classes

--- src/main.rs
+++ src/main.rs
@@ -3,17 +3,16 @@ use std::io;
 //1. To create an object in Rust, you define a custom struct and instantiate it by providing values
 // for its fields. Unlike Traditional OOP languages, Rust doesn;t use classes; instead, you define
 // data in structs and attach behaviors using implementation blocks
-pub struct Room {
+struct Room {
     name: &'static str,
     n: i32,
     e: i32,
     s: i32,
     w: i32,
-    //???
 }
 
 //2. To create an instance of the struct, declare a variable and define the values for each field.
-static HUB: Room = Room {
+const _HOUSTON: Room = Room {
     name: "Houston",
     n: 1,
     e: 2,
