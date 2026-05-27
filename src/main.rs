b559ebdb73197f38add6a29b608e0dbaee94a487
Sl33pySage <52090741+Sl33pySage@users.noreply.github.com>
2026-05-26 19:18:41 -0500

    Initializing the Rooms structs and putting them in an array

--- src/main.rs
+++ src/main.rs
@@ -11,16 +11,55 @@ struct Room {
     w: i32,
 }
 
-//2. To create an instance of the struct, declare a variable and define the values for each field.
-const _HOUSTON: Room = Room {
-    name: "Houston",
-    n: 1,
-    e: 2,
-    s: 3,
-    w: 4,
-};
-
 fn main() {
+    //2. To create an instance of the struct, declare a variable and define the values for each field.
+    const STARTER_ROOM: Room = Room {
+        name: "Plato's cave",
+        n: 1,
+        e: 2,
+        s: 3,
+        w: 4,
+    };
+
+    const FOREST_ROOM: Room = Room {
+        name: "Forest",
+        n: -1,
+        e: 2,
+        s: -1,
+        w: 1,
+    };
+
+    const CITY_ROOM: Room = Room {
+        name: "City",
+        n: 0,
+        e: -1,
+        s: 3,
+        w: -1,
+    };
+
+    const LIBRARY_ROOM: Room = Room {
+        name: "Library",
+        n: 4,
+        e: -1,
+        s: -1,
+        w: -1,
+    };
+
+    /*              GAMEMAP
+     *
+     *          Start/Plato's Cave -- Forest
+     *               |
+     *           City/CITY_ROOM ----- Library
+     *  */
+
+    let _map: [Room; 4] = [STARTER_ROOM, FOREST_ROOM, CITY_ROOM, LIBRARY_ROOM];
+
+    // Start of the game???
+    println!("{}", STARTER_ROOM.name);
+    print!(
+        "Where do you want to go? North:{}\nEast:{}\nSouth:{}\nWest:{}\n",
+        STARTER_ROOM.n, STARTER_ROOM.e, STARTER_ROOM.s, STARTER_ROOM.w
+    );
     println!("Please enter your name:");
 
     let mut name: String = String::new();
